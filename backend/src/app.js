17e7678b7c680f4b6da5b6261af2b5acb177b425
Weslei Dias <49460776+weslei-d@users.noreply.github.com>
2020-09-10 14:55:53 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,18 @@ const routes = require('./routes');
 
 const app = express();
 
+const PORT = process.env.PORT || 8877;
+
+app.listen(PORT, () => {
+  console.log('escutando na porta: '+ PORT);
+})
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'OK'
+  })
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
